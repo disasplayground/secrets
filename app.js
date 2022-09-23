c8b7f9ddfa140f0f064a7e4964cac84665a572a3
disasplayground <disa.tannos@outlook.com>
2022-09-23 16:04:46 +0800

    Add md5 password hash

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
-console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -25,8 +25,6 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 const User = new mongoose.model("user", userSchema);
 
@@ -51,7 +49,7 @@ app.get("/register", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err){
@@ -70,7 +68,7 @@ app.post("/login", function(req, res){
       console.log(err)
     } else {
       if (foundUser) {
-        if (foundUser.password === req.body.password) {
+        if (foundUser.password === md5(req.body.password)) {
           res.render("secrets");
         };
       };
